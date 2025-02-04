88e340cfb65c797c3e5c749c9da0322e25b747f0
krishana-choudhary <krishana87530@gmail.com>
2025-02-05 02:34:47 +0530

    Added convergence test

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -1,9 +1,14 @@
-x=2.0
+x=1000000.0
 s=1.0
-kmax = 10
+tol = 1.0e-14
+kmax = 100
 for k in range(kmax):
     print(f"At iteration {k}, the value of s = {s}")
+    s0 = s
     s=0.5*(s+x/s)
+    delta_s = s - s0
+    if(abs(delta_s/x)  < tol):
+        break
 print(f"finally the value of s={s}")
 
 
