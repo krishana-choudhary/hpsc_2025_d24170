67b53ecf1a2219da3e8393462b862fcb745be66c
krishana-choudhary <krishana87530@gmail.com>
2025-02-08 17:49:32 +0530

    Added a debug flag and return value

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -1,14 +1,18 @@
-def sqrt2(x):
+def sqrt2(x, debug = False):
 	s=1.0
 	tol = 1.0e-14
 	kmax = 100
 	for k in range(kmax):
-		print(f"At iteration {k}, the value of s = {s:20.15f}")
+		if debug:
+			print(f"At iteration {k}, the value of s = {s:20.15f}")
 		s0 = s
 		s=0.5*(s+x/s)
 		delta_s = s - s0
 		if(abs(delta_s/x)  < tol):
 			break
-	print(f"finally the value of s={s:16.15f}")
+	if debug:
+		print(f"finally the value of s={s:16.15f}")
+		
+	return s
 	
 
